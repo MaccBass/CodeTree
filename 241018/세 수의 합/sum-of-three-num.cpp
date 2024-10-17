73d4221e6a52f038de368ef498137e7684545c54
MaccBass <126742414+MaccBass@users.noreply.github.com>
2024-10-18 01:14:38 +0900

    Title: 세 수의 합, Time: 26ms, Memory: 0MB, Status: Passed - CodeTree

--- 241018/세 수의 합/sum-of-three-num.cpp
+++ 241018/세 수의 합/sum-of-three-num.cpp
@@ -1,34 +1,43 @@
 #include <iostream>
 #include <unordered_map>
-#include <vector>
-#include <algorithm>
+
 using namespace std;
 
-vector<int> v;
-unordered_map<int, int> um;
+#define MAX_N 1000
+
+// 변수 선언
+int n, k;
+int arr[MAX_N];
+unordered_map<int, int> freq;
 
 int main() {
-
-    int n, k, ans = 0;
+    // 입력:
     cin >> n >> k;
 
-    for (int i = 0; i < n; i++) {
-        int tmp;
-        cin >> tmp; v.push_back(tmp);
+    int ans = 0;
+
+    // 각 숫자가 몇 번씩 나왔는지를
+    // hashmap에 기록해줍니다.
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+        freq[arr[i]]++;
     }
 
-    sort(v.begin(), v.end());
+    // 배열을 앞에서부터 순회하며 쌍을 만들어줍니다.
+    for(int i = 0; i < n; i++) {
+        // 이미 순회한 적이 있는 숫자는 빼 버림으로서
+        // 같은 조합이 여러번 세어지는 걸 방지합니다.
+        freq[arr[i]]--;
 
-    for (int i = n - 3; i > -1; i--) {
-        int diff = k - v[i];
-
-        for (int j = i + 2; j < n; j++) {
-
-            um[v[i + 1] + v[j]]++;
+        for(int j = 0; j < i; j++) {
+            // 전처리를 해주었기 때문에 이미 순회한 적 있는 값은 hashmap에 없습니다.
+            // 이와 같은 방식으로 같은 조합이 중복되어 세어지는 걸 방지할 수 있습니다.
+            if(freq.count(k - arr[i] - arr[j]) > 0)
+                ans += freq[k - arr[i] - arr[j]];
         }
-        ans += um[diff];
     }
 
     cout << ans;
+
     return 0;
 }
