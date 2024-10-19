389f91e1318ca797ddebe76736699445c13db394
MaccBass <126742414+MaccBass@users.noreply.github.com>
2024-10-19 15:51:34 +0900

    Title: 낮은 지점들, Time: 71ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 241019/낮은 지점들/lowest-points.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+unordered_map<int, int> point;
+
+int main() {
+
+    int n; cin>>n;
+    int ans = 0;
+    while (n){
+
+        int x, y;
+        cin>>x>>y;
+        if (point.find(x) != point.end()){
+            if (point[x] > y) point[x]=y;
+        }
+        else{
+            point[x]=y;
+        }
+
+        n--;
+    }
+
+    for (const auto& i: point){
+        ans += i.second;
+    }
+
+
+    cout<<ans;
+    return 0;
+}
