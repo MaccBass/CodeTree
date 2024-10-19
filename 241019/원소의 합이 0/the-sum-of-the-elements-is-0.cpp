a5d1d5333fec8fcacd489fbe5ef2f2c6c057b889
MaccBass <126742414+MaccBass@users.noreply.github.com>
2024-10-19 14:36:30 +0900

    Title: 원소의 합이 0, Time: 105ms, Memory: 10MB, Status: Passed - CodeTree

--- 241019/원소의 합이 0/the-sum-of-the-elements-is-0.cpp
+++ 241019/원소의 합이 0/the-sum-of-the-elements-is-0.cpp
@@ -1,46 +1,44 @@
 #include <iostream>
 #include <unordered_map>
-#include <vector>
+
 using namespace std;
 
-vector<int> a;
-vector<int> c;
-unordered_map<int, int> ab;
-unordered_map<int, int> cd;
+#define MAX_N 5000
+
+// 변수 선언
+int n;
+int A[MAX_N], B[MAX_N], C[MAX_N], D[MAX_N];
+unordered_map<int, int> freq;
 
 int main() {
-    
-    int n; int cnt=0;
-    cin>>n;
-    for (int i=0;i<n;i++){
-        int tmp; cin>>tmp;
-        a.push_back(tmp);
-    }
+    // 입력:
+    cin >> n;
+    for(int i = 0; i < n; i++) cin >> A[i];
+    for(int i = 0; i < n; i++) cin >> B[i];
+    for(int i = 0; i < n; i++) cin >> C[i];
+    for(int i = 0; i < n; i++) cin >> D[i];
 
-    for (int i=0;i<n;i++){
-        int tmp; cin>>tmp;
-        for (int j=0;j<n;j++){
-            ab[tmp+a[j]]++;
+    long long ans = 0;
+
+    // A 수열에서 숫자 하나, B 수열에서 숫자 하나를 골랐을 때
+    // 나올 수 있는 두 숫자의 합들을 hashmap에 기록해줍니다.
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            freq[A[i] + B[j]]++;
+
+    // C, D 수열을 순회하며 쌍을 만들어줍니다.
+    // 앞서 계산한 hashmap을 이용하면
+    // C, D 수열에서 고른 값으로 A, B와 쌍을 만들 때
+    // 총합이 0이 되는 쌍의 개수를 쉽게 구할 수 있습니다.
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++) {
+            int diff = - C[i] - D[j];
+            if(freq.count(diff) > 0)
+                ans += freq[diff];
         }
-    }
 
-    for (int i=0;i<n;i++){
-        int tmp; cin>>tmp;
-        c.push_back(tmp);
-    }
+    // 출력:
+    cout << ans;
 
-    for (int i=0;i<n;i++){
-        int tmp; cin>>tmp;
-        for (int j=0;j<n;j++){
-            cd[tmp+c[j]]++;
-        }
-    }
-
-    for (const auto& i: ab){
-        int target = 0 - i.first;
-        cnt += i.second * cd[target];
-    }
-
-    cout<<cnt;
     return 0;
 }
