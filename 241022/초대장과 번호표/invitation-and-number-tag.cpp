b7d7860ffc907d734a3555a6bb39f6963ba98886
MaccBass <126742414+MaccBass@users.noreply.github.com>
2024-10-22 01:44:13 +0900

    Title: 초대장과 번호표, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241022/초대장과 번호표/invitation-and-number-tag.cpp
+++ 241022/초대장과 번호표/invitation-and-number-tag.cpp
@@ -23,7 +23,9 @@ int main() {
         int k; cin >> k;
         // k가 1명: 바로 초대, 2명: 첫번째 초대받고 1명남으니 자동 초대
         if (k < 3) {
-            int tmp; cin >> tmp; invitation.push(tmp);
+            for (int i = 0; i < k; i++) {
+                int tmp; cin >> tmp; invitation.push(tmp);
+            }
         }
         // k가 3명 이상: 첫번째는 초대, 나머지는 set으로 만들어 vector에 넣음.
         else {
