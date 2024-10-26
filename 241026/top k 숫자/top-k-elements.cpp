42e8fbb91baae0389a09c9bb115a3f7698400562
MaccBass <126742414+MaccBass@users.noreply.github.com>
2024-10-26 14:04:33 +0900

    Title: top k 숫자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241026/top k 숫자/top-k-elements.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    set<int> s;
+    int n, k; cin>>n>>k;
+    for (int i=0;i<n;i++){
+        int tmp; cin>>tmp; s.insert(tmp);
+    }
+
+    auto it = s.rbegin();
+    for (int i=0;i<k;i++){
+        cout<<*it<<' ';
+        it--;
+        
+    }
+    return 0;
+}
