c459171ab829addb8d65122218ff46b86558d5e8
MaccBass <126742414+MaccBass@users.noreply.github.com>
2024-11-03 22:46:43 +0900

    Title: 최대로 연속한 숫자, Time: 439ms, Memory: 10MB, Status: Passed - CodeTree

--- 241103/최대로 연속한 숫자/maximum-consecutive-number.cpp
+++ 241103/최대로 연속한 숫자/maximum-consecutive-number.cpp
@@ -1,75 +1,62 @@
 #include <iostream>
 #include <set>
-#include <map>
-#define MAX 2147000000
+#include <tuple>
+
 using namespace std;
 
-set<pair<int, int>> s;
-// key: size, value: s내의 개수
-map<int, int, greater<int>> counts;
+// 변수 선언
+int n, m;
+set<int> s_num;
+set<tuple<int, int, int> > s_len; // (-길이, 시작 숫자, 끝 숫자)
 
 int main() {
+    // 입력:
+    cin >> n >> m;
 
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int n, m; cin >> n >> m;
-    int ans;
+    // s_num : 지워진 숫자 모음(코드의 깔끔한 처리를 위해
+    // 범위 밖의 숫자를 treeset에 추가했습니다)
+    s_num.insert(-1);
+    s_num.insert(n + 1);
+    // s_len : (-구간의 길이, 시작 숫자, 끝 숫자)
+    // 길이가 긴 구간부터 나오도록 구간의 길이에 -를 붙여서 넣어줍니다.
+    s_len.insert(make_tuple(-(n + 1), -1, n + 1));
 
-    s.insert(make_pair(0, n));
-    counts[n+1]++;
-    ans = counts.begin()->first;
-    // dummy
-    s.insert(make_pair(n + 1, n + 1));
+    for(int i = 0; i < m; i++) {
+        // 숫자를 입력받습니다.
+        int y;
+        cin >> y;
 
-    for (int i = 0; i < m; i++) {
-        int ans = 0;
-        int sizetmp;
-        int num; cin >> num;
+        // 입력받은 숫자를 treeset에 추가합니다.
+        s_num.insert(y);
 
-        // num이 들어간 부분집합 찾기
-        auto itmp = s.upper_bound(make_pair(num, 0));
-        pair<int, int> p = *itmp;
-        // num으로 시작하는게 아니라면 그 직전꺼에 포함됨.
-        if (num != p.first) {
-            p = *(prev(itmp));
-        }
-        // 짜를 집합의 사이즈
-        sizetmp = p.second - p.first + 1;
-
-
-        // 집합 자르기
-        if (p.first == num) {
-            s.insert(make_pair(num + 1, p.second));
-            s.erase(p);
-
-            counts[sizetmp]--;
-            if (counts[sizetmp] == 0) counts.erase(sizetmp);
-            counts[sizetmp-1]++;
-        }
-        if (p.second == num) {
-            s.insert(make_pair(p.first, num - 1));
-            s.erase(p);
-            
-            counts[sizetmp]--;
-            if (counts[sizetmp] == 0) counts.erase(sizetmp);
-            counts[sizetmp-1]++;
-        }
-        if (p.first < num && num < p.second) {
-            s.insert(make_pair(p.first, num - 1));
-            s.insert(make_pair(num + 1, p.second));
-            s.erase(p);
-
-            counts[sizetmp]--;
-            if (counts[sizetmp] == 0) counts.erase(sizetmp);
-            int small = num-1 - p.first + 1;
-            int big = p.second - (num+1) + 1;
-            counts[small]++;
-            counts[big]++;
-        }
-
+        // 입력받은 숫자 y를 기준으로
+        // 그 바로 뒤의 숫자를 z, 바로 앞의 숫자를 x라고 두었습니다.
+        set<int>::iterator it;
+        it = s_num.find(y);
+        it++;
+        int z = *it;
         
-        ans = counts.begin()->first;
+        it = s_num.find(y);
+        it--;
+        int x = *it;
+        
+        // 구간의 길이는 (x ~ z) 구간이 사라지며,
+        // (x ~ y) 구간과 (y ~ z) 구간이 새로 생겨납니다.
+        s_len.erase(make_tuple(
+            -(z - x - 1), x, z
+        ));
+        s_len.insert(make_tuple(
+            -(y - x - 1), x, y
+        ));
+        s_len.insert(make_tuple(
+            -(z - y - 1), y, z
+        ));
+        
 
-        cout << ans << '\n';
+        // y가 추가된 후로 구간 중 가장 긴 구간을 찾아 출력합니다.
+        int best_length;
+        tie(best_length, ignore, ignore) = *s_len.begin();
+        cout << -best_length << endl;
     }
 
     return 0;
