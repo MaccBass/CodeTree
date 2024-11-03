4bffc51c6293353c0ac9057f22c683990fe4fe42
MaccBass <126742414+MaccBass@users.noreply.github.com>
2024-11-03 19:45:13 +0900

    Title: 최대로 연속한 숫자, Time: 1580ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 241103/최대로 연속한 숫자/maximum-consecutive-number.cpp
+++ 241103/최대로 연속한 숫자/maximum-consecutive-number.cpp
@@ -3,50 +3,45 @@
 #define MAX 2147000000
 using namespace std;
 
-set<set<int>> ss;
+set<pair<int, int>> s;
 
 int main() {
 
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     int n, m; cin >> n >> m;
 
-    set<int> setinit;
-    for (int i = 0; i <= n; i++) {
-        setinit.insert(i);
-    }
-    ss.insert(setinit);
-
+    s.insert(make_pair(0, n));
+    s.insert(make_pair(n + 1, n + 1));
     for (int i = 0; i < m; i++) {
-        int tmpsize = 0;
-
-        int numToErase; cin >> numToErase;
-        for (auto& itss : ss) {
-            set<int> settmp = itss;
-            auto it = settmp.find(numToErase);
-            if (it != settmp.end()) {
-                set<int> setLess, setGreater;
-                for (auto& i : settmp) {
-                    if (i < numToErase) {
-                        setLess.insert(i);
-                    }
-                    else if (i > numToErase) {
-                        setGreater.insert(i);
-                    }
-                    else continue;
-                }
-                ss.insert(setLess); ss.insert(setGreater);
-                ss.erase(settmp);
-                break;
-            }
+        int ans = 0;
+        int num; cin >> num;
+        auto itmp = s.upper_bound(make_pair(num, 0));
+        pair<int, int> p = *itmp;
+        if (num != p.first) {
+            p = *(prev(itmp));
         }
 
-        for (auto& itss : ss) {
-            if (tmpsize < itss.size()) {
-                tmpsize = itss.size();
-            }
+        if (p.first == num) {
+            s.insert(make_pair(num + 1, p.second));
+            s.erase(p);
+        }
+        if (p.second == num) {
+            s.insert(make_pair(p.first, num - 1));
+            s.erase(p);
+        }
+        if (p.first < num && num < p.second) {
+            s.insert(make_pair(p.first, num - 1));
+            s.insert(make_pair(num + 1, p.second));
+            s.erase(p);
         }
 
-        cout << tmpsize << '\n';
+        for (auto& p2 : s) {
+            int tmp = p2.second - p2.first + 1;
+            if (ans < tmp) ans = tmp;
+        }
+
+        cout << ans << '\n';
     }
+
     return 0;
 }
