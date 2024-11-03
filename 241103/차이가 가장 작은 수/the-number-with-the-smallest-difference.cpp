34f3dd291151e4b84c042014c3a33962721122c3
MaccBass <126742414+MaccBass@users.noreply.github.com>
2024-11-03 15:11:47 +0900

    Title: 차이가 가장 작은 수, Time: 19ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241103/차이가 가장 작은 수/the-number-with-the-smallest-difference.cpp
+++ 241103/차이가 가장 작은 수/the-number-with-the-smallest-difference.cpp
@@ -19,15 +19,15 @@ int main() {
 
     for (auto it=s.begin(); it != s.end(); it++){
         auto it2 = next(it);
+        if (it2 == s.end()) break;
         int tmp = MAX;
         
         // 차가 m보다 처음으로 커질때까지 it2 증가
-        while ((*it2 - *it) < m && it2 != s.end()){
+        while (it2 != s.end() && (*it2 - *it) < m){
             it2++;
         }
 
         // 끝까지 가도 m보다 큰 경우가 안나오므로, it보다 더 큰수와 맨 끝수를 빼도 m보다 작음.
-        if (it2 == s.end()) break;
         
         // it를 증가시키며 it~it2 범위까지 가장 작은 차를 찾음
         for (it; (*it2-*it) >= m; it++){
@@ -36,7 +36,7 @@ int main() {
         // 갱신
         if (tmp < ans && tmp >= m) ans = tmp;
         if (ans == m) break;
-        it = it2;
+        it = prev(it2);
 
     }
 
