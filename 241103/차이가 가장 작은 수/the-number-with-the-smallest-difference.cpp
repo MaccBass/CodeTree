a885f4c677e0e87767845383526a9005a83b91d7
MaccBass <126742414+MaccBass@users.noreply.github.com>
2024-11-03 14:46:04 +0900

    Title: 차이가 가장 작은 수, Time: 17ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241103/차이가 가장 작은 수/the-number-with-the-smallest-difference.cpp
+++ 241103/차이가 가장 작은 수/the-number-with-the-smallest-difference.cpp
@@ -11,50 +11,33 @@ int main() {
     int ans = MAX;
     cin >> n >> m;
 
-    while (n) {
-        int num; cin >> num; s.insert(num);
-        set<int>::iterator it = s.find(num);
-
-        if (it != s.begin()) {
-            set<int>::iterator it2 = prev(it);
-            for (it2; it2 != s.end(); it2--) {
-                int tmp = *it - *it2;
-                // m보다 작으면 계속 탐색
-                if (tmp < m) continue;
-
-                // ans보다 작지 않으면 탐색 종료
-                if (tmp >= ans) break;
-                else { ans = tmp; break; }
-            }
-        }
-
-        if (next(it) != s.end()) {
-            set<int>::iterator it2 = next(it);
-            for (it2; it2 != s.end(); it2++) {
-                int tmp = *it2 - *it;
-                if (tmp < m) continue;
-
-                if (tmp >= ans) break;
-                else { ans = tmp; break; }
-            }
-        }
-
-        --n;
+    while (n){
+        int tmp; cin>>tmp;
+        s.insert(tmp);
+        n--;
     }
 
-    for (set<int>::iterator it = s.begin(); it != s.end(); it++) {
-        for (set<int>::iterator it2 = next(it); it2 != s.end(); it2++) {
-            int tmp = *it2 - *it;
-            if (tmp < m) continue;
-            if (tmp >= ans) {
-                break;
-            }
-            else {
-                ans = tmp; break;
-            }
+    for (auto it=s.begin(); it != s.end(); it++){
+        auto it2 = next(it);
+        int tmp;
+        
+        // 차가 m보다 처음으로 커질때까지 it2 증가
+        while ((*it2 - *it) < m && it2 != s.end()){
+            it2++;
         }
 
-        if (m == ans) break;
+        // 끝까지 가도 m보다 큰 경우가 안나오므로, it보다 더 큰수와 맨 끝수를 빼도 m보다 작음.
+        if (it2 == s.end()) break;
+        
+        // it를 증가시키며 it~it2 범위까지 가장 작은 차를 찾음
+        for (it; (*it2-*it) >= m; it++){
+            tmp = *it2 - *it;
+        }
+        // 갱신
+        if (tmp < ans && tmp >= m) ans = tmp;
+        if (ans == m) break;
+        it = prev(it2);
+
     }
 
     if (ans == MAX) cout << -1;
