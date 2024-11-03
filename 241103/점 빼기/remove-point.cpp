e95ba12cac93296e45ad2edfa1c8831b17dd4a93
MaccBass <126742414+MaccBass@users.noreply.github.com>
2024-11-03 16:59:42 +0900

    Title: 점 빼기, Time: 37ms, Memory: 1MB, Status: Passed - CodeTree

--- 241103/점 빼기/remove-point.cpp
+++ 241103/점 빼기/remove-point.cpp
@@ -5,6 +5,7 @@ using namespace std;
 set<pair<int, int>> points;
 
 int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     int n, m; cin>>n>>m;
 
     for (int i=0;i<n;i++){
