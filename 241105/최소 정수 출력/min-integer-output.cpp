a6260a788138f31045d0a91e0801dac16ad8e8ba
MaccBass <126742414+MaccBass@users.noreply.github.com>
2024-11-05 17:44:25 +0900

    Title: 최소 정수 출력, Time: 40ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 241105/최소 정수 출력/min-integer-output.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+priority_queue<int, vector<int>, greater<int>> pq;
+
+int main() {
+    
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int n; cin>>n;
+    while(n){
+        int num; cin>>num;
+
+        if (num == 0){
+            if (pq.empty()){
+                cout<<0<<'\n';
+            }
+            else{
+                cout<<pq.top()<<'\n';
+                pq.pop();
+            }
+        }
+
+        else{
+            pq.push(num);
+        }
+
+        n--;
+    }
+}
