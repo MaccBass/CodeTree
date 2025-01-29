67db8e05f9cf309d219ef4d476b4c8765742e21e
MaccBass <126742414+MaccBass@users.noreply.github.com>
2025-01-29 15:22:57 +0900

    Title: 연결리스트 1, Time: 106ms, Memory: 14MB, Status: Passed - CodeTree

--- 250129/연결리스트 1/linked-list1.cpp
+++ 250129/연결리스트 1/linked-list1.cpp
@@ -6,6 +6,8 @@ using namespace std;
 list<string> l;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
     string s_init; cin >> s_init;
     l.push_back(s_init);
