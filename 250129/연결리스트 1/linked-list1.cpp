349c41bf904d9cd87fef5ad02c25db65f5d4b7af
MaccBass <126742414+MaccBass@users.noreply.github.com>
2025-01-29 14:26:35 +0900

    Title: 연결리스트 1, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250129/연결리스트 1/linked-list1.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <list>
+#include <string>
+
+using namespace std;
+list<string> l;
+
+int main() {
+
+    string s_init; cin >> s_init;
+    l.push_back(s_init);
+    auto cur = l.begin();
+    int n; cin >> n;
+
+    while (n) {
+
+        int method; cin >> method;
+        string s_value;
+        switch (method) {
+        case 1:
+            cin >> s_value;
+            if (cur != l.begin()) {
+                l.insert(prev(cur), s_value);
+            }
+            else {
+                l.push_front(s_value);
+            }
+            break;
+
+        case 2:
+            cin >> s_value;
+            l.insert(next(cur), s_value);
+            break;
+
+        case 3:
+            if (cur != l.begin()) {
+                cur = prev(cur);
+            }
+            break;
+
+        case 4:
+            if (next(cur) != l.end()) {
+                cur = next(cur);
+            }
+        }
+
+        if (cur != l.begin()) {
+            cout << *prev(cur) << ' ';
+        }
+        else {
+            cout << "(Null) ";
+        }
+        cout << *cur << ' ';
+        if (next(cur) != l.end()) {
+            cout << *next(cur) << '\n';
+        }
+        else {
+            cout << "(Null)\n";
+        }
+        n--;
+    }
+
+    cout << '\n';
+    auto it = l.begin();
+    while (it != l.end()) {
+        cout << *it << ' ';
+        it++;
+    }
+    return 0;
+}
