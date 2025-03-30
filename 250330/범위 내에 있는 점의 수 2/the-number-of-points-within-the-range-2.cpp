9cf921250b61e32957dfb6172e599bd35b2f6b6e
MaccBass <126742414+MaccBass@users.noreply.github.com>
2025-03-30 18:38:39 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 170ms, Memory: 8MB, Status: Passed - Codetree

--- 250330/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.cpp
+++ 250330/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.cpp
@@ -6,18 +6,16 @@ int main() {
     int N, Q;
     int line[1000001] = {0,};
     int prefix[1000001] = {0,};
-    int LINE_MAX=0;
     cin>>N>>Q;
 
     for (int i=0; i<N; i++){
         int tmp; cin>>tmp;
         line[tmp]=1;
 
-        if (tmp > LINE_MAX) LINE_MAX = tmp;
     }
 
     prefix[0] = line[0];
-    for (int i=1; i<=LINE_MAX; i++){
+    for (int i=1; i<=1000000; i++){
         prefix[i] = line[i] + prefix[i-1];
     }
 
